78ecd018f65ab37ca9b47a596401b57feea395f4
tromboman <ezublena@gmail.com>
2016-10-06 20:09:56 -0300

    correct

---
+++ app/Services/Login.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilianozublena
+ * Date: 6/10/16
+ * Time: 8:08 PM
+ */
+
+namespace App;
+
+
+class Login
+{
+
+}
---
+++ app/Services/ServiceInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: emilianozublena
+ * Date: 6/10/16
+ * Time: 8:07 PM
+ */
+
+namespace App;
+
+
+interface ServiceInterface
+{
+
+}
